f71c7f5d5cb7c364dcd228dae58d7fe9eaba1578
Daniel Nelson <dnelson@bluejade.com>
2011-09-27 07:59:54 -0500

    using inline formatting for bolding

--- prawn_issue_263_bis.rb
+++ prawn_issue_263_bis.rb
@@ -13,7 +13,7 @@ Prawn::Document.generate "prawn_issue_263_example.pdf" do |pdf|
   # Comment this line in order to print the second cell in bold
   pdf.fallback_fonts ["Calibri"]
   
-  pdf.table [["Following cell must be bold", "PDF for ISSUE 263"]], cell_style: { height: 60 } do
+  pdf.table [["Following cell must be bold", "<b>PDF for ISSUE 263</b>"]], cell_style: { height: 60, :inline_format => true } do
     row(0).column(0).width = 115
     row(0).column(1).width = 230
     
@@ -26,7 +26,7 @@ Prawn::Document.generate "prawn_issue_263_example.pdf" do |pdf|
   
   pdf.bounding_box [pdf.bounds.left, pdf.bounds.top - 82], width: pdf.bounds.width do
     # This cell contains some π glyphes that the default Helvetica font can't print: Calibri is needed
-    question_cell = "Which is the L value if L is the subject of the formulae 2πfL = x?\n\nA L = 2πf\nB L = 2πf / x\nC L = x / 2πf"
-    pdf.table [["1", question_cell]]
+    question_cell = "<b>Which is the L value if L is the subject of the formulae 2πfL = x?</b>\n\nA L = 2πf\nB L = 2πf / x\nC L = x / 2πf"
+    pdf.table [["1", question_cell]], cell_style: { height: 60, :inline_format => true }
   end
 end
